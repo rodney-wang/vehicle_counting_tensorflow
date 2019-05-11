6c68343ec8aa47bcea4a528f11068183dceae86a
rodney-wang <wfei@aibee.com>
2019-05-10 19:15:15 -0700

    cropping images based on detection results

---
+++ utils/filter_detection_box_crop.py
@@ -0,0 +1,176 @@
+import cv2
+import os
+import collections
+import numpy as np
+
+
+STANDARD_COLORS = [
+    'AliceBlue', 'Chartreuse', 'Aqua', 'Aquamarine', 'Azure', 'Beige', 'Bisque',
+    'BlanchedAlmond', 'BlueViolet', 'BurlyWood', 'CadetBlue', 'AntiqueWhite',
+    'Chocolate', 'Coral', 'CornflowerBlue', 'Cornsilk', 'Crimson', 'Cyan',
+    'DarkCyan', 'DarkGoldenRod', 'DarkGrey', 'DarkKhaki', 'DarkOrange',
+    'DarkOrchid', 'DarkSalmon', 'DarkSeaGreen', 'DarkTurquoise', 'DarkViolet',
+    'DeepPink', 'DeepSkyBlue', 'DodgerBlue', 'FireBrick', 'FloralWhite',
+    'ForestGreen', 'Fuchsia', 'Gainsboro', 'GhostWhite', 'Gold', 'GoldenRod',
+    'Salmon', 'Tan', 'HoneyDew', 'HotPink', 'IndianRed', 'Ivory', 'Khaki',
+    'Lavender', 'LavenderBlush', 'LawnGreen', 'LemonChiffon', 'LightBlue',
+    'LightCoral', 'LightCyan', 'LightGoldenRodYellow', 'LightGray', 'LightGrey',
+    'LightGreen', 'LightPink', 'LightSalmon', 'LightSeaGreen', 'LightSkyBlue',
+    'LightSlateGray', 'LightSlateGrey', 'LightSteelBlue', 'LightYellow', 'Lime',
+    'LimeGreen', 'Linen', 'Magenta', 'MediumAquaMarine', 'MediumOrchid',
+    'MediumPurple', 'MediumSeaGreen', 'MediumSlateBlue', 'MediumSpringGreen',
+    'MediumTurquoise', 'MediumVioletRed', 'MintCream', 'MistyRose', 'Moccasin',
+    'NavajoWhite', 'OldLace', 'Olive', 'OliveDrab', 'Orange', 'OrangeRed',
+    'Orchid', 'PaleGoldenRod', 'PaleGreen', 'PaleTurquoise', 'PaleVioletRed',
+    'PapayaWhip', 'PeachPuff', 'Peru', 'Pink', 'Plum', 'PowderBlue', 'Purple',
+    'Red', 'RosyBrown', 'RoyalBlue', 'SaddleBrown', 'Green', 'SandyBrown',
+    'SeaGreen', 'SeaShell', 'Sienna', 'Silver', 'SkyBlue', 'SlateBlue',
+    'SlateGray', 'SlateGrey', 'Snow', 'SpringGreen', 'SteelBlue', 'GreenYellow',
+    'Teal', 'Thistle', 'Tomato', 'Turquoise', 'Violet', 'Wheat', 'White',
+    'WhiteSmoke', 'Yellow', 'YellowGreen'
+]
+
+
+def py_cpu_nms(dets, thresh):
+    """Pure Python NMS baseline."""
+    x1 = dets[:, 0]
+    y1 = dets[:, 1]
+    x2 = dets[:, 2]
+    y2 = dets[:, 3]
+    scores = dets[:, 4]  # bbox打分
+
+    areas = (x2 - x1 + 1) * (y2 - y1 + 1)
+    # 打分从大到小排列，取index
+    order = scores.argsort()[::-1]
+    # keep为最后保留的边框
+    keep = []
+    while order.size > 0:
+        # order[0]是当前分数最大的窗口，肯定保留
+        i = order[0]
+        keep.append(i)
+        # 计算窗口i与其他所有窗口的交叠部分的面积
+        xx1 = np.maximum(x1[i], x1[order[1:]])
+        yy1 = np.maximum(y1[i], y1[order[1:]])
+        xx2 = np.minimum(x2[i], x2[order[1:]])
+        yy2 = np.minimum(y2[i], y2[order[1:]])
+
+        w = np.maximum(0.0, xx2 - xx1 + 1)
+        h = np.maximum(0.0, yy2 - yy1 + 1)
+        inter = w * h
+        # 交/并得到iou值
+        ovr = inter / (areas[i] + areas[order[1:]] - inter)
+        # inds为所有与窗口i的iou值小于threshold值的窗口的index，其他窗口此次都被窗口i吸收
+        inds = np.where(ovr <= thresh)[0]
+        # order里面只保留与窗口i交叠面积小于threshold的那些窗口，由于ovr长度比order长度少1(不包含i)，所以inds+1对应到保留的窗口
+        order = order[inds + 1]
+
+    return keep
+
+
+def crop_boxes_from_image(bname,
+                              crop_folder,
+                              image,
+                              boxes,
+                              classes,
+                              scores,
+                              category_index,
+                              instance_masks=None,
+                              keypoints=None,
+                              use_normalized_coordinates=False,
+                              max_boxes_to_draw=20,
+                              min_score_thresh=.2,
+                              agnostic_mode=False,
+                              line_thickness=4):
+    """Overlay labeled boxes on an image with formatted scores and label names.
+
+    This function groups boxes that correspond to the same location
+    and creates a display string for each detection and overlays these
+    on the image. Note that this function modifies the image in place, and returns
+    that same image.
+
+    Args:
+      image: uint8 numpy array with shape (img_height, img_width, 3)
+      boxes: a numpy array of shape [N, 4]
+      classes: a numpy array of shape [N]. Note that class indices are 1-based,
+        and match the keys in the label map.
+      scores: a numpy array of shape [N] or None.  If scores=None, then
+        this function assumes that the boxes to be plotted are groundtruth
+        boxes and plot all boxes as black with no classes or scores.
+      category_index: a dict containing category dictionaries (each holding
+        category index `id` and category name `name`) keyed by category indices.
+      instance_masks: a numpy array of shape [N, image_height, image_width], can
+        be None
+      keypoints: a numpy array of shape [N, num_keypoints, 2], can
+        be None
+      use_normalized_coordinates: whether boxes is to be interpreted as
+        normalized coordinates or not.
+      max_boxes_to_draw: maximum number of boxes to visualize.  If None, draw
+        all boxes.
+      min_score_thresh: minimum score threshold for a box to be visualized
+      agnostic_mode: boolean (default: False) controlling whether to evaluate in
+        class-agnostic mode or not.  This mode will display scores but ignore
+        classes.
+      line_thickness: integer (default: 4) controlling line width of the boxes.
+
+    Returns:
+      uint8 numpy array with shape (img_height, img_width, 3) with overlaid boxes.
+    """
+    # Create a display string (and color) for every box location, group any boxes
+    # that correspond to the same location.
+    csv_line_util = "not_available"
+    counter = 0
+    is_vehicle_detected = []
+    box_to_display_str_map = collections.defaultdict(list)
+    box_to_score = collections.defaultdict(float)
+    box_to_instance_masks_map = {}
+    box_to_keypoints_map = collections.defaultdict(list)
+    if not max_boxes_to_draw:
+        max_boxes_to_draw = boxes.shape[0]
+    for i in range(min(max_boxes_to_draw, boxes.shape[0])):
+        if scores is None or scores[i] > min_score_thresh:
+            box = tuple(boxes[i].tolist())
+            if instance_masks is not None:
+                box_to_instance_masks_map[box] = instance_masks[i]
+            if keypoints is not None:
+                box_to_keypoints_map[box].extend(keypoints[i])
+            if scores is None:
+                box_to_color_map[box] = 'black'
+            else:
+                if not agnostic_mode:
+                    if classes[i] in category_index.keys():
+                        class_name = category_index[classes[i]]['name']
+                    else:
+                        class_name = 'N/A'
+                    display_str = '{}: {}%'.format(class_name, int(100 * scores[i]))
+                else:
+                    display_str = 'score: {}%'.format(int(100 * scores[i]))
+
+                box_to_display_str_map[box].append(display_str)
+                if agnostic_mode:
+                    box_to_color_map[box] = scores[i]
+                else:
+                    box_to_color_map[box] = STANDARD_COLORS[
+                        classes[i] % len(STANDARD_COLORS)]
+
+    # Write out all boxes into image.
+    icount = 0
+    boxes_filtered =[]
+    for box, color in box_to_color_map.items():
+        ymin, xmin, ymax, xmax = box
+        im_height, im_width, channel = image.shape
+        if use_normalized_coordinates:
+            (left, right, top, bottom) = (int(xmin * im_width), int(xmax * im_width),
+                                          int(ymin * im_height), int(ymax * im_height))
+
+        display_str_list = box_to_display_str_map[box]
+        print display_str_list
+        # we are interested just vehicles (i.e. cars and trucks)
+        if (("car" in display_str_list[0]) or ("truck" in display_str_list[0]) or ("bus" in display_str_list[0])):
+            print(xmin, ymin, xmax, ymax)
+            crop_path = '%s/%s_%02d.jpg'%(crop_folder, bname, icount)
+            cv2.imwrite(crop_path, image[top:bottom, left:right])
+            boxes_filtered.append([])
+            icount += 1
+
+    return boxes_filtered
+
---
+++ vehicle_detection_crop.py
@@ -0,0 +1,140 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# ----------------------------------------------
+# --- Author         : Fei Wang
+# --- Mail           : rodney.wang@gmail.com
+# --- Date           : 27th January 2018
+# ----------------------------------------------
+
+# Imports
+import numpy as np
+import os
+import six.moves.urllib as urllib
+import sys
+import tensorflow as tf
+import cv2
+import numpy as np
+import argparse
+import glob
+
+# Object detection imports
+from utils import label_map_util
+from utils import filter_detection_box_crop
+
+
+if tf.__version__ < '1.4.0':
+    raise ImportError('Please upgrade your tensorflow installation to v1.4.* or later!'
+                      )
+
+# Variables
+total_passed_vehicle = 0  # using it to count vehicles
+
+# By default I use an "SSD with Mobilenet" model here. See the detection model zoo (https://github.com/tensorflow/models/blob/master/research/object_detection/g3doc/detection_model_zoo.md) for a list of other models that can be run out-of-the-box with varying speeds and accuracies.
+# What model to download.
+MODEL_NAME = 'ssd_mobilenet_v1_coco_2017_11_17'
+MODEL_FILE = MODEL_NAME + '.tar.gz'
+DOWNLOAD_BASE = \
+    'http://download.tensorflow.org/models/object_detection/'
+
+# Path to frozen detection graph. This is the actual model that is used for the object detection.
+PATH_TO_CKPT = MODEL_NAME + '/frozen_inference_graph.pb'
+
+# List of the strings that is used to add correct label for each box.
+PATH_TO_LABELS = os.path.join('data', 'mscoco_label_map.pbtxt')
+
+NUM_CLASSES = 90
+
+# Download Model
+# uncomment if you have not download the model yet
+# Load a (frozen) Tensorflow model into memory.
+detection_graph = tf.Graph()
+with detection_graph.as_default():
+    od_graph_def = tf.GraphDef()
+    with tf.gfile.GFile(PATH_TO_CKPT, 'rb') as fid:
+        serialized_graph = fid.read()
+        od_graph_def.ParseFromString(serialized_graph)
+        tf.import_graph_def(od_graph_def, name='')
+
+# Loading label map
+# Label maps map indices to category names, so that when our convolution network predicts 5, we know that this corresponds to airplane. Here I use internal utility functions, but anything that returns a dictionary mapping integers to appropriate string labels would be fine
+label_map = label_map_util.load_labelmap(PATH_TO_LABELS)
+categories = label_map_util.convert_label_map_to_categories(label_map,
+        max_num_classes=NUM_CLASSES, use_display_name=True)
+category_index = label_map_util.create_category_index(categories)
+
+
+# Helper code
+def load_image_into_numpy_array(image):
+    (im_width, im_height) = image.size
+    return np.array(image.getdata()).reshape((im_height, im_width,
+            3)).astype(np.uint8)
+
+
+# Detection
+def object_detection_function(frame, img_basename, crop_folder):
+    total_passed_vehicle = 0
+
+    with detection_graph.as_default():
+        with tf.Session(graph=detection_graph) as sess:
+
+            # Definite input and output Tensors for detection_graph
+            image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
+
+            # Each box represents a part of the image where a particular object was detected.
+            detection_boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
+
+            # Each score represent how level of confidence for each of the objects.
+            # Score is shown on the result image, together with the class label.
+            detection_scores = detection_graph.get_tensor_by_name('detection_scores:0')
+            detection_classes = detection_graph.get_tensor_by_name('detection_classes:0')
+            num_detections = detection_graph.get_tensor_by_name('num_detections:0')
+
+
+            input_frame = frame
+
+            # Expand dimensions since the model expects images to have shape: [1, None, None, 3]
+            image_np_expanded = np.expand_dims(input_frame, axis=0)
+
+            # Actual detection.
+            (boxes, scores, classes, num) = \
+                sess.run([detection_boxes, detection_scores,
+                         detection_classes, num_detections],
+                         feed_dict={image_tensor: image_np_expanded})
+
+            print("Number of boxes = {}".format(len(boxes)))
+            # Visualization of the results of a detection.
+
+            boxes_filtered=filter_detection_box_crop.crop_boxes_from_image(
+                                img_basename,
+                                crop_folder,
+                                input_frame,
+                                np.squeeze(boxes),
+                                np.squeeze(classes).astype(np.int32),
+                                np.squeeze(scores),
+                                category_index,
+                                use_normalized_coordinates=True,
+                                line_thickness=4,
+                                )
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('-l', '--data_folder', default='/Users/fei/data/parking/香港车牌')
+    parser.add_argument('-c', '--crop_folder', default='/Users/fei/data/parking/hk_cropped_20190509', help='cropped car image folder')
+    args = parser.parse_args()
+
+    if not os.path.exists(args.crop_folder):
+          os.makedirs(args.crop_folder)
+    img_all = glob.glob(os.path.join(args.data_folder, "*.JPG"))
+    img_all.sort()
+
+    for idx, fname in enumerate(img_all):
+        print fname
+        if idx > 5:
+            break
+        bname = os.path.splitext(os.path.basename(fname))[0]
+        #fname ='/Users/fei/data/parking/香港车牌/IMG_1536.JPG'
+        frame = cv2.imread(fname)
+        print frame.shape
+        object_detection_function(frame, bname, args.crop_folder)
